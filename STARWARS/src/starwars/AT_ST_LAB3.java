690f754594c4295fc0b53eea08d3c8eacff59bf6
davidcr01 <davidcorreafjb2001@gmail.com>
2022-12-31 18:52:21 +0100

    Entrando en mundo, hace MOVEIN y CAPTURE pero los demás no

--- STARWARS/src/starwars/AT_ST_LAB3.java
+++ STARWARS/src/starwars/AT_ST_LAB3.java
@@ -18,6 +18,7 @@ public class AT_ST_LAB3 extends AT_ST_LAB2 {
     String sessionAlias = "milanesa";
     ACLMessage controller;
 
+    @Override
     public void setup() {
         this.defSessionAlias(sessionAlias);
 
@@ -37,6 +38,8 @@ public class AT_ST_LAB3 extends AT_ST_LAB2 {
             case JOINSESSION:
                 myStatus = MyJoinSession();
                 break;
+            case SELECTGOAL:
+                myStatus = MySelectGoal();
             case SOLVEPROBLEM:
                 myStatus = MySolveProblem();
                 break;
@@ -48,6 +51,135 @@ public class AT_ST_LAB3 extends AT_ST_LAB2 {
                 doExit();
                 break;
         }
+        
+    }
+    
+    @Override
+    public AT_ST_FULL.Status MovementGoal(String city){
+        behaviour = AgPlan(E, A);
+        // Si se ha completado el plan se avanza de objetivo
+        if (behaviour == null || behaviour.isEmpty()) {
+            if ("MOVEIN".equals(current_goal[0])){
+                if (getEnvironment().getCurrentCity().equals(current_goal[1])){
+                    Info("Goal " + current_goal[0] + " " + current_goal[1] + " has been solved!");
+                    getEnvironment().setNextGoal();
+                    return AT_ST_FULL.Status.SELECTGOAL;
+                }
+	    } else { // "MOVEBY <droidship>".equals(current_goal)
+	    	if (getEnvironment().getCurrentCity().equals(city)){
+                    Info("Goal " + current_goal[0] + " " + current_goal[1] + " has been solved!");
+                    getEnvironment().setNextGoal();
+                    return AT_ST_FULL.Status.SELECTGOAL;
+                }
+	    }
+            Alert("Found no plan to execute");
+            return AT_ST_FULL.Status.CLOSEPROBLEM;
+        // Si hay acciones por realizar del plan
+        } else {// Execute
+            Info("Found plan: " + behaviour.toString());
+            while (!behaviour.isEmpty()) {
+                a = behaviour.get(0);
+                behaviour.remove(0);
+                Info("Excuting " + a);
+                this.MyExecuteAction(a.getName());
+                if(getEnvironment().getEnergy() < EnergyLimitToAskRecharge){
+                    EnergyRecharge();
+                }
+                if (!Ve(E)) {
+                    this.Error("The agent is not alive: " + E.getStatus());
+                    return AT_ST_FULL.Status.CLOSEPROBLEM;
+                }
+            }
+            this.MyReadPerceptions();
+            return MovementGoal(city);   // función iterativa
+        }
+    }
+    
+    @Override
+    public AT_ST_FULL.Status MySolveProblem(){
+        if (getEnvironment().getCurrentMission().isOver()){
+            Info("The problem is over");
+            Message("The problem " + problem + " has been solved");
+            return this.SelectMission();
+        }
+        
+        // Obtenemos el objetivo actual. Los objetivos van separados por espacios
+        // por lo que los splitteamos por espacios
+        current_goal = getEnvironment().getCurrentGoal().split(" ");
+  
+        // Si el objetivo es movernos, utilizamos en asistente de LARVA
+        // para desplazarnos y se realiza el algoritmo de movimiento
+        if (current_goal[0].equals("MOVEIN")){
+            if (!getEnvironment().getCurrentCity().equals(current_goal[1])){
+                outbox = session.createReply();
+                outbox.setContent("Request course in " + current_goal[1] + " session " + sessionKey);
+                
+                // Cambio performativa y conversational id
+                outbox.setPerformative(ACLMessage.REQUEST);
+                outbox.setConversationId(sessionKey);
+                
+                this.LARVAsend(outbox);
+                session = this.LARVAblockingReceive();
+                getEnvironment().setExternalPerceptions(session.getContent());
+                return MovementGoal("");
+            } else {
+                Info("Goal " + current_goal[0] + " " + current_goal[1] + " has been solved!");
+                return Status.SELECTGOAL;
+            }  
+        // Si el objetivo es listar, obtenemos la lista de personas dado un tipo
+        // y se avanza al siguiente objetivo
+        } else if (current_goal[0].equals("LIST")){
+            doQueryPeople(current_goal[1]);
+            Info("Goal " + current_goal[0] + " " + current_goal[1] + " " + current_goal[2] + " has been solved!");
+            
+            return Status.SELECTGOAL;
+        } else if (current_goal[0].equals("REPORT")){
+            doReportTypeGoal(current_goal[1]);
+            Info("Goal " + current_goal[0] + " " + current_goal[1] + " has been solved!");
+            return Status.SELECTGOAL;
+        } else if (current_goal[0].equals("CAPTURE")){
+            doCapture(current_goal[1], current_goal[2]);
+            Info("Goal " + this.getEnvironment().getCurrentGoal() + " has been solved!");
+            return Status.SELECTGOAL;
+        } else if (current_goal[0].equals("MOVEBY")){
+            if (destCity.equals("")){
+                destCity = getMoveByCity(current_goal[1]);
+                return MovementGoal(destCity);
+            } else {
+                Info("Goal " + current_goal[0] + " " + current_goal[1] + " has been solved!");
+                return Status.SELECTGOAL;
+            }
+        } else if (current_goal[0].equals("TRANSFER")){
+            Info("Estoy en el transfer");
+            System.out.println(getEnvironment().getCurrentGoal());
+            doTransfer();
+            
+            Info("Goal " + this.getEnvironment().getCurrentGoal() + " has been solved!");
+            return Status.SELECTGOAL;            
+        }
+        
+        return Status.EXIT;
+    } 
+    
+    public Status MySelectGoal() {
+        Info("ESTOY EN EL SELECT GOALLLL");
+        if(getEnvironment().getCurrentMission()==null){
+            Info("No current mission, selecting...");
+            return this.SelectMission();
+        }
+        else{
+            Info("Setting next goal...");
+            getEnvironment().setNextGoal();
+
+            //En caso de haber acabado nuestra misión, esperamos indicaciones del SSD
+            if (getEnvironment().getCurrentMission().isOver()) {
+                Info("Mission is over, selecting...");
+                return this.SelectMission();
+            }
+        }
+
+        return Status.SOLVEPROBLEM;
+
     }
     
     @Override
@@ -98,21 +230,23 @@ public class AT_ST_LAB3 extends AT_ST_LAB2 {
                 break;
             }
         }
-        System.out.println("Session key"+sessionKey);
+        System.out.println("Session key: "+sessionKey);
                 
         current_city = "Whitehorse";
+        
+        outbox = new ACLMessage();
+        outbox.setSender(this.getAID());
+        outbox.addReceiver(new AID(sessionManager, AID.ISLOCALNAME));
+        
+        // Iniciamos sesión en las coordenadas de la ciudad seleccionada
+        outbox.setContent("Request join session " + sessionKey + " in " +  current_city);
+        // Cambio performativa y conversational id
+        outbox.setPerformative(ACLMessage.REQUEST);
+        outbox.setConversationId(sessionKey);
         
         this.resetAutoNAV();
         this.DFAddMyServices(new String[]{"TYPE ITT"});
         this.DFAddMyServices(new String[]{"TEAM " + sessionAlias});
-        outbox = session.createReply();
-        
-        // Iniciamos sesión en las coordenadas de la ciudad seleccionada
-        outbox.setContent("Request join session " + sessionKey + " in " +  current_city);
-        
-        // Cambio performativa y conversational id
-        outbox.setPerformative(ACLMessage.REQUEST);
-        outbox.setConversationId(sessionKey);
         
         this.LARVAsend(outbox);
         session = this.LARVAblockingReceive();
@@ -121,8 +255,13 @@ public class AT_ST_LAB3 extends AT_ST_LAB2 {
             Error("Could not join session " + sessionKey + " due " + session.getContent());
             return AT_ST_FULL.Status.CLOSEPROBLEM;
         }
+        
+        outbox = session.createReply();
+          
+        // Antes de seleccionar la misión es necesario actualizar las percepciones
+        this.MyReadPerceptions();
 
-        return SelectMission();
+        return Status.SELECTGOAL;
     }
         
         
@@ -158,10 +297,19 @@ public class AT_ST_LAB3 extends AT_ST_LAB2 {
             this.getEnvironment().resetCourse();
             return Status.SOLVEPROBLEM;
         } else if (controller.getPerformative() == ACLMessage.CANCEL){
+            Info("Cancel received");
             return Status.CHECKOUT;
         } else {
             return Status.CHECKOUT;
         }
+    }
+    
+    public void informBoss(){
+        controller.createReply();
+        controller.setContent(E.getCurrentGoal());
+        controller.setConversationId(sessionKey);
+        controller.setPerformative(ACLMessage.INFORM_REF);
+        this.LARVAsend(outbox);
     }
 }
 
